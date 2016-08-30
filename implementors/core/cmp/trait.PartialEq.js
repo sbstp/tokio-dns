43ceea319682528e1ebf76e5cd3b3446e7da4484
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2016-08-30 12:51:49 -0400

    Update docs

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tokio_dns"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='tokio_dns/enum.Endpoint.html' title='tokio_dns::Endpoint'>Endpoint</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
